599f5d875cf076f286723e059f154f91aa9f65d0
Andrei Mihăilă <andrei.mihaila@gmail.com>
2017-05-29 15:18:39 +0300

    Added some escaping to the tags, fields and measurements

--- lib/influx_ex/writer.ex
+++ lib/influx_ex/writer.ex
@@ -13,18 +13,34 @@ defmodule InfluxEx.Writer do
     |> Connection.write(db_name, config)
   end
 
+  # Escapes commas (,), equal signs (=) and spaces with \
+  defp escape_ces(v) do
+    Regex.replace(~r/([,= ])/, "#{v}", "\\\\\\1")
+  end
+
+  # Escapes commas (,) and spaces with \
+  defp escape_cs(v) do
+    Regex.replace(~r/([, ])/, "#{v}", "\\\\\\1")
+  end
+
+  # Escapes double quotes (") with \
+  defp escape_dc(v) do
+    Regex.replace(~r/(["])/, "#{v}", "\\\\\\1")
+  end
 
   @spec string_for_point(map) :: String.t
   defp string_for_point(data = %{measurement: measurement, fields: fields}) do
     tags = data[:tags] || %{}
     time = data[:time]
 
-    fields_string = Enum.map(fields, fn {key, val} -> "#{key}=#{val}" end)
-    |> Enum.join(",")
+    fields_string =
+      fields
+      |> Enum.map(fn {key, val} -> "#{escape_ces(key)}=#{escape_dc(val)}" end)
+      |> Enum.join(",")
 
-    tags_strings = Enum.map(tags, fn {key, val} -> "#{key}=#{val}" end)
+    tags_strings = Enum.map(tags, fn {key, val} -> "#{escape_ces(key)}=#{escape_ces(val)}" end)
 
-    key = Enum.join([measurement | tags_strings], ",")
+    key = Enum.join([escape_cs(measurement) | tags_strings], ",")
 
     "#{key} #{fields_string} #{time}"
     |> String.strip
